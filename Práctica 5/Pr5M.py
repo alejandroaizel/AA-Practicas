a158a194d06ee6a6a51f257788ea31f10552b807
Mimel <miguelrobledoblanco@gmail.com>
2020-06-19 16:33:32 +0200

    Pr 5

--- Práctica 5/Pr5M.py
+++ Práctica 5/Pr5M.py
@@ -20,6 +20,7 @@ def cost_reg(Theta, XX, Y, Landa):
     dif = np.sum((Theta.dot(XX.T) - Y)**2)
     cost = (1 / 2 * len(XX)) * dif 
     return cost
+
 def error(Theta,XX,y,landa):   
     H = np.dot(XX, Theta)
     aux = (H - y) ** 2
@@ -28,28 +29,23 @@ def error(Theta,XX,y,landa):
 
 def add_features(X, p):
     for i in range(2,p+1):
-       x1 = X[:,1]**i 
-       X = np.hstack((X, np.reshape(x1,(x1.shape[0],1))))
+        x1 = X[:,1]**i 
+        X = np.hstack((X, np.reshape(x1,(x1.shape[0],1))))
     return X
 
 def normalize(X):
-    X_norm = np.zeros_like(X)
-    X_norm[:,0] = X[:,0]
-    for i in range(1,X.shape[1]):
-        mu = np.mean(X[:,i])
-        sigma = np.std(X[:,i])
-        data = (X[:,i]-mu) / sigma
-        X_norm[:,i] = data
-    return X_norm, mu, sigma
+    Mu = np.mean(X[:,1:], axis=0)
+    Sigma = np.std(X[:,1:],axis=0)
+    X_norm = (X[:,1:] - Mu) / Sigma
+    X_norm = np.hstack((np.ones((len(X), 1)), X_norm))
+
+    return [X_norm, Mu, Sigma]
 
 def normalize_val_or_test(X,mu,sigma):
-    X_norm = np.zeros_like(X)
-    X_norm[:,0] = X[:,0]
-    for i in range(1,X.shape[1]):
-        data = (X[:,i]-mu) / sigma
-        X_norm[:,i] = data
-
+    X_norm = (X[:,1:] - mu) / sigma
+    X_norm = np.hstack((np.ones((len(X), 1)), X_norm))
     return X_norm
+
 def show_graph(X, Y, Theta, name='graph'):
 
     X_l = [np.amin(X), np.amax(X)]
@@ -63,24 +59,18 @@ def show_graph(X, Y, Theta, name='graph'):
     plt.savefig('Práctica 5/Recursos/{}.png'.format(name), dpi=200)
 
 
-def show_graph_normalized(X, Y, theta, mu , sigma, name = 'graph_normalized'):
+def show_graph_normalized(X, Y,X_norm,theta, name = 'graph_normalized'):
 
-    x_pts = np.linspace(-60, 40, 12)
-    y_pts = theta[0] * np.ones(12)
-    for i in range(1,9):
-        y_pts += theta[i] * (x_pts**i - mu[i-1]) / sigma[i-1]
-
+    Y_l = X_norm*theta
 
     plt.figure()
-    plt.plot(X[1], Y, 'rx', ms=8)
-    plt.plot(x_pts, y_pts, 'b--')
-    plt.xlim(-60,50)
-    plt.ylim(-10,60)
+    plt.scatter(X.T[1], Y, c='r', marker='x', s=40)
+    #plt.plot(X, Y_l, c='b')
     plt.xlabel('Change in water level (x)')
     plt.ylabel('Water flowing out of the dam (y)')
     plt.savefig('Práctica 5/Recursos/{}.png'.format(name), dpi=200)
 
-def show_learning_curve(Train, val,num_examples):
+def show_learning_curve(Train, val,num_examples, name = 'Learning curve'):
 
 
     plt.figure()
@@ -89,7 +79,7 @@ def show_learning_curve(Train, val,num_examples):
     plt.xlabel('Number of training examples')
     plt.ylabel('Error')
     plt.legend()
-    plt.savefig('Práctica 5/Recursos/{}.png'.format('Learning Curve'), dpi=200)
+    plt.savefig('Práctica 5/Recursos/{}.png'.format(name), dpi=200)
 
 def learning_curve(Theta, X, y, Xval, yval, landa):
 
@@ -98,8 +88,8 @@ def learning_curve(Theta, X, y, Xval, yval, landa):
     for i in range(0,len(X)):
         fmin = minimize(fun=error,x0=Theta,args=(X[0:i],y[0:i],landa ),method='TNC',jac=False) 
         theta = fmin['x']
-        train_error[i] = cost_reg(theta, X[:i],y[:i],landa)
-        val_error[i] = cost_reg(theta, Xval,yval,landa) 
+        train_error[i] = error(theta, X[:i],y[:i],landa)
+        val_error[i] = error(theta, Xval,yval,landa) 
 
     return train_error, val_error
 
@@ -117,11 +107,13 @@ def main():
 
     Theta = np.ones(2)
     Landa = 1
-    #fmin = minimize(fun=cost_and_gradient,x0=Theta,args=(X,y,Landa),method='TNC',jac=True)
-    #show_graph(X,y,fmin['x'])
+    fmin = minimize(fun=cost_and_gradient,x0=Theta,args=(X,y,Landa),method='TNC',jac=True)
+    show_graph(X,y,fmin['x'])
 
-    #error_train, error_val = learning_curve(Theta,X,y,Xval,yval,0)
-    #show_learning_curve(error_train,error_val, len(X))
+    error_train, error_val = learning_curve(Theta,X,y,Xval,yval,0)
+    show_learning_curve(error_train,error_val, len(X))
+
+    
 
     X_featurized = add_features(X,8)
     Xval_featurized = add_features(Xval,8)
@@ -132,18 +124,21 @@ def main():
     Xtest_normalized = normalize_val_or_test(Xtest_featurized,mu,sigma)
     Theta_featurized= np.ones(9)
 
-    #fmin_norm =  minimize(fun=cost_and_gradient,x0=Theta_featurized,args=(X_normalized,y,0),method='TNC',jac=True)
+    # TODO 
+    fmin_norm =  minimize(fun=cost_and_gradient,x0=Theta_featurized,args=(X_normalized,y,0),method='TNC',jac=True)
+    show_graph_normalized(X,y,X_normalized,fmin_norm['x'],name='Graph normalized')
 
-    #show_graph_normalized(X,y, fmin_norm['x'],mu, sigma)
-
+    error_train, error_val = learning_curve(Theta_featurized,X_normalized,y,Xval_normalized,yval,0)
+    show_learning_curve(error_train,error_val, len(X), name = 'Learning curve for polynomial hypotesis')
+    # TODO lo que esta entre los todos no va del todo
     Landas = [0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]
     errors_train = []
     errors_val = []
     for landa in Landas:
         res = minimize(fun=cost_and_gradient,x0=Theta_featurized,args=(X_normalized,y,landa),method='TNC', jac=True);
         theta = res['x']
-        errors_train.append(cost_reg(theta, X_normalized, y, landa))
-        errors_val.append(cost_reg(theta, Xval_normalized, yval, landa))
+        errors_train.append(error(theta, X_normalized, y, landa))
+        errors_val.append(error(theta, Xval_normalized, yval, landa))
     
     plt.figure()
     plt.plot(Landas, errors_train,'b', label='Train')
