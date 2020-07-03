4133611bed202d3058e49cd6f48a03a5500e65fb
Mimel <miguelrobledoblanco@gmail.com>
2020-07-03 20:03:01 +0200

    :v

--- Práctica Final/PrFinalM.py
+++ Práctica Final/PrFinalM.py
@@ -44,6 +44,14 @@ def cost_reg(Theta, X, Y, Landa):
 def cost_gradient_reg(Theta, X, Y, Landa):
     return [cost_reg(Theta, X, Y, Landa), gradient_reg(Theta, X, Y, Landa)]
 
+def cost_netword(output_layer, y, M):
+    return 1 / M * np.trace((np.dot(-y, np.log(output_layer)) - np.dot((1 - y), np.log(1 - output_layer))))
+
+def cost_reg_network(output_layer, y, theta1, theta2, M, reg):
+    H = cost_netword(output_layer, y, M)
+
+    return H + reg / (2 * M) * (np.sum(theta1[:, 1:] * theta1[:, 1:]) + np.sum(theta2[:, 1:] * theta2[:, 1:]))
+
 def normalize(X):
     Mu = np.mean(X, axis=0)
     Sigma = np.std(X,axis=0)
@@ -51,26 +59,81 @@ def normalize(X):
 
     return [X_norm, Mu, Sigma]
 
-def visualize_boundary(X, y, svm, name ='visualize boundary'):
-    x1 = np.linspace(X[:, 0].min()-0.1, X[:, 0].max(), 100)
-    x2 = np.linspace(X[:, 1].min()-0.1, X[:, 1].max(), 100)
-
-    x1, x2 = np.meshgrid(x1, x2)    
-    yp = svm.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape) 
-
-    pos = (y == 1).ravel()    
-    neg = (y == 0).ravel()    
-
-    plt.figure()    
-    plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')    
-    plt.scatter(X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')    
-    plt.contour(x1, x2, yp)    
-    plt.savefig('Práctica Final/Recursos/{}.png'.format(name), dpi=200)
-    plt.close()
-
 def normal_ecuation_method(X, Y):
     return np.matmul(np.matmul(np.linalg.pinv(np.matmul(np.transpose(X), X)), np.transpose(X)), Y)
 
+def sigmoid(X):
+    return 1 / (1 + np.exp(-X))
+
+def random_weights(L_in, L_out, E_ini=0.12):
+    return np.random.uniform(-E_ini, E_ini, (L_in, L_out))
+
+def forward_propagate(theta1, theta2, X):
+
+    hidden_layer_z2 = np.matmul(X, np.transpose(theta1))
+    hidden_layer_a2 = sigmoid(hidden_layer_z2)
+    hidden_layer_a2 = np.hstack([np.ones([np.shape(hidden_layer_a2)[0], 1]), hidden_layer_a2])
+
+    output_layer_z3 = np.matmul(hidden_layer_a2, np.transpose(theta2))
+    output_layer_a3 = sigmoid(output_layer_z3)
+
+    return X, hidden_layer_z2, hidden_layer_a2, output_layer_z3, output_layer_a3
+
+def backprop(params_rn, num_entries, num_hidden, num_labels, X, y, reg):
+    theta1 = np.reshape(params_rn[:num_hidden * (num_entries + 1)], (num_hidden, (num_entries + 1)))
+    theta2 = np.reshape(params_rn[num_hidden * (num_entries + 1):], (num_labels, (num_hidden + 1)))
+    M = len(y)
+
+    X = np.hstack([np.ones([len(X), 1]), X])
+
+    a1, z2, a2, z3, h = forward_propagate(theta1, theta2, X)
+
+    cost = cost_reg_network(np.transpose(h), y, theta1, theta2, M, reg)
+
+    m = X.shape[0]
+    delta1, delta2 = np.zeros(np.shape(theta1)), np.zeros(np.shape(theta2))
+
+    for t in range(m):
+        a1t = a1[t, :]
+        a2t = a2[t, :]
+        ht = h[t,:]
+        yt = y[t]
+        d3t = ht-yt
+        d2t = np.dot(theta2.T, d3t) * (a2t * (1 - a2t))
+        delta1 = delta1 + np.dot(d2t[1:, np.newaxis], a1t[np.newaxis, :])
+        delta2 = delta2 + np.dot(d3t[:, np.newaxis], a2t[np.newaxis, :])
+
+    gradient1 = delta1 / m
+    gradient2 = delta2 / m
+
+    reg1 = (reg / m) * theta1
+    reg2 = (reg / m) * theta2
+
+    reg1[:, 0] = 0
+    reg2[:, 0] = 0
+
+    gradient1 += reg1
+    gradient2 += reg2
+
+    gradient = np.concatenate((np.ravel(gradient1), np.ravel(gradient2)))
+
+    return cost, gradient
+
+def correct_classified(X, y, params_rn, num_entries, num_hidden, num_labels):
+    X = np.hstack([np.ones([len(X), 1]), X])
+    theta1 = np.reshape(params_rn[:num_hidden * (num_entries + 1)], (num_hidden, (num_entries + 1)))
+    theta2 = np.reshape(params_rn[num_hidden * (num_entries + 1):], (num_labels, (num_hidden + 1)))
+
+    hidden_layer = sigmoid(np.matmul(theta1, np.transpose(X)))
+    hidden_layer = np.vstack([np.ones([len(X)]), hidden_layer])
+
+    output_layer = sigmoid(np.matmul(theta2, hidden_layer))
+
+    max_values = np.argmax(output_layer, axis=0)
+
+    correct_values = y == max_values
+
+    print("Se han clasificado correctamente: {} %".format(np.sum(correct_values) / len(correct_values) * 100))
 
 #1
 def lineal_regresion(X_test,Y_test, X_val, Y_val):
@@ -96,7 +159,24 @@ def logistic_regresion(X_test,Y_test, X_val, Y_val):
 
 #3
 def neural_network(X_test,Y_test, X_val, Y_val):
-    print()
+   
+    X_test, mu, sigma = normalize(X_test)
+    input_layer_size = 20
+    hidden_layer_size = 6
+    output_layer_size = 1
+
+    theta1 = random_weights(hidden_layer_size-1, input_layer_size)
+    theta2 = random_weights(output_layer_size,hidden_layer_size)
+
+    params_rn = np.concatenate((theta1.flatten(), theta2.flatten()))
+
+    Y_test = np.reshape(Y_test,(len(Y_test),1))
+
+    fmin = minimize(fun=backprop, x0=params_rn, args=(input_layer_size -1, hidden_layer_size-1, 1, X_test, Y_test, 1), method='TNC', jac=True, options={'maxiter': 70})
+   
+    print('El coste es de: {}'.format(fmin['fun']))
+
+    correct_classified(X_test, Y_test, fmin.x, input_layer_size -1, hidden_layer_size-1, 1)
 
 #4
 def SVM(X_test,Y_test, X_val, Y_val):
@@ -131,7 +211,7 @@ def split_data(Data):
 
     total_examples, features = X.shape
 
-    test_size = int(0.7 * total_examples)
+    test_size = int(0.4 * total_examples)
     val_size= total_examples-test_size
 
     X_test = X[0:test_size, :]
@@ -145,15 +225,15 @@ def split_data(Data):
 def main():
     
 
-    Data = load_csv('Práctica Final/Recursos/high_diamond_ranked_10min.csv')
+    Data = load_csv('Práctica Final/Recursos/high_diamond_ranked.csv')
 
     X_test, X_val, Y_test, Y_val = split_data(Data)
 
     
-    lineal_regresion(X_test,Y_test, X_val, Y_val)
-    logistic_regresion(X_test,Y_test, X_val, Y_val)
+    #lineal_regresion(X_test,Y_test, X_val, Y_val)
+    #logistic_regresion(X_test,Y_test, X_val, Y_val)
     neural_network(X_test,Y_test, X_val, Y_val)
-    SVM(X_test,Y_test, X_val, Y_val)
+    #SVM(X_test,Y_test, X_val, Y_val)
 
 
 
